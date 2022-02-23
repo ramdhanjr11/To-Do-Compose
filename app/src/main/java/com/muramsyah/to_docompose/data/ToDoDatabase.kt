e5e63c959db204cabd35dddafc54581ab20703b8
M Ramdhan Syahputra <ramdhanjr690@gmail.com>
2022-02-23 08:23:44 +0700

    Setup Dagger Hilt

--- app/src/main/java/com/muramsyah/to_docompose/data/ToDoDatabase.kt
+++ app/src/main/java/com/muramsyah/to_docompose/data/ToDoDatabase.kt
@@ -1,9 +1,10 @@
 package com.muramsyah.to_docompose.data
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 import com.muramsyah.to_docompose.data.models.ToDoTask
 
 @Database(entities = [ToDoTask::class], version = 1, exportSchema = false)
-abstract class ToDoDatabase {
+abstract class ToDoDatabase : RoomDatabase(){
     abstract fun todoDao(): TodoDao
 }
