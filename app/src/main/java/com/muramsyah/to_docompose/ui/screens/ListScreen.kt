ff6a41e46819cb72bc5eb6abea21ba8408a5d3fc
M Ramdhan Syahputra <ramdhanjr690@gmail.com>
2022-03-05 16:55:31 +0700

    List App Bar Actions

--- app/src/main/java/com/muramsyah/to_docompose/ui/screens/ListScreen.kt
+++ app/src/main/java/com/muramsyah/to_docompose/ui/screens/ListScreen.kt
@@ -2,6 +2,7 @@ package com.muramsyah.to_docompose.ui.screens
 
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -10,20 +11,25 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.muramsyah.to_docompose.R
+import com.muramsyah.to_docompose.ui.theme.fabBackgroundColor
 
 @Composable
-fun ListScreen(navigateToTaskScreen: (Int) -> Unit) {
+fun ListScreen(navigateToTaskScreen: (taskId: Int) -> Unit) {
     Scaffold(
+        topBar = { ListAppBar() },
         content = {},
         floatingActionButton = { ListFab(onFabClicked = navigateToTaskScreen) }
     )
 }
 
 @Composable
-fun ListFab(onFabClicked: (Int) -> Unit) {
-    FloatingActionButton(onClick = {
-        onFabClicked(-1)
-    }) {
+fun ListFab(onFabClicked: (taskId: Int) -> Unit) {
+    FloatingActionButton(
+        onClick = {
+            onFabClicked(-1)
+        },
+        backgroundColor = MaterialTheme.colors.fabBackgroundColor
+    ) {
         Icon(
             imageVector = Icons.Default.Add,
             contentDescription = stringResource(id = R.string.add_button),
