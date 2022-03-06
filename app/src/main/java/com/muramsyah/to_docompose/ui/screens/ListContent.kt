ee5c9323cc8f8457ad4aa829ce0ed5d107214366
M Ramdhan Syahputra <ramdhanjr690@gmail.com>
2022-03-06 12:18:19 +0700

    Display All Tasks - Design Task Item

---
+++ app/src/main/java/com/muramsyah/to_docompose/ui/screens/ListContent.kt
@@ -0,0 +1,91 @@
+package com.muramsyah.to_docompose.ui.screens
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import com.muramsyah.to_docompose.data.models.Priority
+import com.muramsyah.to_docompose.data.models.ToDoTask
+import com.muramsyah.to_docompose.ui.theme.*
+
+@Composable
+fun ListContent() {
+
+}
+
+@ExperimentalMaterialApi
+@Composable
+fun TaskItem(
+    toDoTask: ToDoTask,
+    navigateToTaskScreen: (taskId: Int) -> Unit
+) {
+    Surface(
+        modifier = Modifier.fillMaxWidth(),
+        color = MaterialTheme.colors.taskItemBackgroundColor,
+        shape = RectangleShape,
+        elevation = TASK_ITEM_ELEVATION,
+        onClick = {
+            navigateToTaskScreen(toDoTask.id)
+        }
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(all = LARGE_PADDING)
+                .fillMaxWidth()
+        ) {
+            Row {
+                Text(
+                    modifier = Modifier.weight(8f),
+                    text = toDoTask.title,
+                    color = MaterialTheme.colors.taskItemTextColor,
+                    style = MaterialTheme.typography.h5,
+                    fontWeight = FontWeight.Bold,
+                    maxLines = 1
+                )
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f),
+                    contentAlignment = Alignment.TopEnd
+                ) {
+                    Canvas(modifier = Modifier.size(PRIORITY_INDICATOR_SIZE)) {
+                        drawCircle(
+                            color = toDoTask.priority.color
+                        )
+                    }
+                }
+            }
+            Text(
+                text = toDoTask.description,
+                color = MaterialTheme.colors.taskItemTextColor,
+                style = MaterialTheme.typography.subtitle1,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+        }
+    }
+}
+
+@ExperimentalMaterialApi
+@Composable
+@Preview
+fun TaskItemPreview() {
+    TaskItem(
+        toDoTask = ToDoTask(
+            0,
+            "Title",
+            "Some Random Text Here",
+            priority = Priority.MEDIUM
+        ),
+        navigateToTaskScreen = {}
+    )
+}
